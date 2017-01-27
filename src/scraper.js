b5a1c9e7f3df07f51750078d735582d24262535b
skylocke <mathematical.fiction@gmail.com>
2017-01-26 16:09:39 -0800

    added json of hitler quotes

---
+++ src/scraper.js
@@ -0,0 +1,20 @@
+var request = require("request");
+var cheerio = require("cheerio");
+var fs = require("fs");
+
+var URL = "https://www.goodreads.com/author/quotes/30691.Adolf_Hitler";
+
+var hitler_quotes;
+
+request(URL, function(error, response, body) {
+  var $ = cheerio.load(body);
+  var quoteTexts = $('.quoteText')
+
+  hitler_quotes = quoteTexts.map(function(index, element) {
+    return element.children[0].data.trim().replace("“", "").replace("”", "");
+  }).get();
+  fs.writeFile('hitler_quotes.json', JSON.stringify(hitler_quotes), 'utf8', function (err) {
+    console.error(err);
+  });
+
+});
